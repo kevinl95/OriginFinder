5d5189212a151874b17174a1d5c42f2c0e78a4bc
kloeffler95 <kevin.loeffler@colorado.edu>
2025-03-22 20:56:12 -0600

    Adding barcode reader.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-import { AppBar, AppBarSection, AppBarSpacer } from '@progress/kendo-react-layout';
+import React, { useState } from 'react';
+import { AppBar, AppBarSection, AppBarSpacer, Card, CardHeader, CardBody } from '@progress/kendo-react-layout';
+import BarcodeReader from 'react-barcode-reader';
+import './index.scss';
+import './App.css'
 
 function App() {
+
+  const [data, setData] = useState('Not Found');
+
+  const handleScan = (result) => {
+    if (result) {
+      setData(result);
+    }
+  };
+
+  const handleError = (err) => {
+    console.error(err);
+  };
+
   return (
     <div className="App">
       <AppBar>
@@ -9,6 +26,23 @@ function App() {
         </AppBarSection>
         <AppBarSpacer />
       </AppBar>
+
+      <div className="content">
+        <Card style={{ backgroundSize: 'cover', color: 'white' }}>
+          <CardHeader>
+            <h2>Welcome to Origin Finder</h2>
+          </CardHeader>
+          <CardBody>
+            <p>Find the country of origin of a product from its UPC code.</p>
+            <BarcodeReader
+              onError={handleError}
+              onScan={handleScan}
+            />
+            <p>Scanned Code: {data}</p>
+          </CardBody>
+        </Card>
+      </div>
+
       <AppBar position="bottom">
         <AppBarSection>
           <p>Origin Finder</p>
