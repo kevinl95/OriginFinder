2127f50ce8e27c47ea1f633ec7c6d774045e2609
kloeffler95 <kevin.loeffler@colorado.edu>
2025-03-23 14:45:09 -0600

    Adding countries list and cleaning up UI.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Card, CardHeader, CardBody } from '@progress/kendo-react-layout';
 import { Typography } from '@progress/kendo-react-common';
 import { Button } from '@progress/kendo-react-buttons';
 import { BarcodeScanner, useTorch } from 'react-barcode-scanner'
+import getCountryFromUPC from './countries';
 import "react-barcode-scanner/polyfill"
 import './index.scss';
 import './App.css'
@@ -14,8 +15,14 @@ function App() {
   const [scanning, setScanning] = useState(false);
   const [isSupportTorch, isOpen, setOpen] = useTorch()
 
-  const startScanning = () => {
-    setScanning(true);
+  const toggleScanning = () => {
+    if (!scanning) {
+      setScanning(true);
+      return;
+    } else {
+      setScanning(false);
+      setData('Not Found');
+    }
   };
 
   const onTorchSwitch = () => {
@@ -30,15 +37,13 @@ function App() {
         </AppBarSection>
         <AppBarSpacer />
       </AppBar>
-
-      <div className="content">
-        <Typography.p>Find the country of origin of a product from its UPC code.</Typography.p>
+      <Typography.h3>Find the country of origin of a product from its UPC code.</Typography.h3>
+      <div className="content" style={{ width: '80%', margin: '0 auto' }}>
         <Card style={{ backgroundSize: 'cover', color: 'white' }}>
           <CardHeader>
           <Typography.h2>Welcome to Origin Trackr</Typography.h2>
           </CardHeader>
           <CardBody>
-            <Button onClick={startScanning}>Start Scanning</Button>
             {scanning && (
               <BarcodeScanner 
                 options={{ formats: ['upc_a', 'upc_e', 'ean_13', 'ean_8'] }}
@@ -46,14 +51,19 @@ function App() {
                 {...isSupportTorch ? <Button onClick={onTorchSwitch}>Turn on Flashlight</Button>: null}
               />
             )}
+            {!scanning && (
+              <Button themeColor="success" id="scanbutton" onClick={toggleScanning} style={{ fontSize: '1.5rem', padding: '1rem 2rem' }}>Start Scanning</Button>
+            )}
           </CardBody>
         </Card>
-        <Typography.p>Scanned Code: {data}</Typography.p>
+        <br></br>
       </div>
+      <Typography.p>Scanned Code: {data}</Typography.p>
+      <Typography.p>Country of Origin: {getCountryFromUPC(data)}</Typography.p>
 
       <AppBar position="bottom">
         <AppBarSection>
-          <Typography.h2>Origin Trackr</Typography.h2>
+          <Typography.p>Origin Trackr</Typography.p>
         </AppBarSection>
         <AppBarSpacer />
       </AppBar>
